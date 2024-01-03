752ce690db37bbf038ce8bde9122a8aac8c17ad8
AlejandroSZIOSA <ggato33@gmail.com>
2024-01-03 20:54:17 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,21 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "@radix-ui/themes/styles.css";
 import { Flex, Text, Button } from "@radix-ui/themes";
+import DropdownTest from "./components/DropdownTest.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Text>Hola</Text>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+
+        <Button>Let's go</Button>
+      </Flex>
+    </div>
   );
 }
 
